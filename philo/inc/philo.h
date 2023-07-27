128bee73d8f703af6c5a030049e5d1b92b664494
esettes <ioanaroxana.st@gmail.com>
2023-07-27 23:48:43 +0200

    update

---
+++ philo/inc/philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iostancu <iostancu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/27 23:45:28 by iostancu          #+#    #+#             */
+/*   Updated: 2023/07/27 23:46:16 by iostancu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <pthread.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/time.h>
+
+#endif
