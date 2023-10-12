29c985a70fa7021afbe4fbde37a140ae0586b000
esettes <ioanaroxana.st@gmail.com>
2023-10-13 00:03:38 +0200

    start philo again

---
+++ philo_2/inc/philo.h
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iostancu <iostancu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/12 22:13:09 by iostancu          #+#    #+#             */
+/*   Updated: 2023/10/12 23:56:22 by iostancu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <pthread.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <stdint.h>
+
+# define RED_		"\x1b[31m"
+# define GREEN_		"\x1b[32m"
+# define YELLOW_	"\x1b[33m"
+# define BLUE_		"\x1b[34m"
+# define VIOLET_	"\x1b[35m"
+# define CYAN_		"\x1b[36m"
+# define RESET_		"\x1b[0m"
+
+# define FORK		"has taken a fork"
+# define EAT		"is eating"
+# define SLEEP		"is sleeping"
+# define THINK		"is thinking"
+# define DIE		"died"
+# define ALLOC_ERR	"Allocation error"
+
+struct	s_data;
+
+typedef struct s_philo
+{
+	struct s_data	*data;
+	pthread_t		*tid;
+	int				id;
+	size_t			r_fork;
+	size_t			l_fork;
+	uint64_t		times_eaten;
+	size_t			is_die;
+	uint64_t		start_eating;
+	pthread_mutex_t	mut;
+}				t_philo;
+
+typedef struct s_data
+{
+	t_philo			*philos;
+	pthread_mutex_t	*forks;
+	pthread_t		controller;
+	pthread_mutex_t	mut_write;
+	pthread_mutex_t	mut_start;
+	int				num_philos;
+	uint64_t		start_time;
+	uint64_t		t_to_die;
+	uint64_t		t_to_eat;
+	uint64_t		t_to_sleep;
+	uint64_t		many_times_to_eat;
+}				t_data;
+
+
+int			init_data(t_data **data, int philos, u_int64_t sleep, u_int64_t eat,
+					u_int64_t die, int times_to_eat);
+int			init_philos(t_data *data);
+
+uint64_t	ft_atoi(const char *str);
+char		*ft_itoa(uint64_t n);
+
+void		ft_putstrc_fd(char *color, char *s, int fd);
+void		ft_putendlc_fd(char *color, char *s, int fd);
+
+void		ft_exit(t_data **data);
+uint64_t	get_time(void);
+
+#endif
---
+++ philo_2/src/init.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iostancu <iostancu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/12 22:21:43 by iostancu          #+#    #+#             */
+/*   Updated: 2023/10/13 00:03:12 by iostancu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	init_data(t_data **data, int philos, u_int64_t sleep, u_int64_t eat,
+		u_int64_t die, int times_to_eat)
+{
+	int	i;
+
+	i = 0;
+	(*data) = malloc(sizeof(t_data));
+	if (!*data)
+	{
+		ft_putendlc_fd(RED_, ALLOC_ERR, 1);
+		return (EXIT_FAILURE);
+	}
+	(*data)->num_philos = philos;
+	(*data)->forks = malloc(sizeof(pthread_mutex_t) * ((*data)->num_philos));
+	if (!(*data)->forks)
+	{
+		ft_putendlc_fd(RED_, ALLOC_ERR, 1);
+		return (EXIT_FAILURE);
+	}
+	while (i < (*data)->num_philos)
+	{
+		if (pthread_mutex_init(&(*data)->forks[i], NULL) != 0)
+		{
+			ft_putendlc_fd(RED_, "Forks init error", 1);
+			return (EXIT_FAILURE);
+		}
+		i++;
+	}
+	(*data)->t_to_die = die;
+	(*data)->t_to_eat = eat;
+	(*data)->t_to_sleep = sleep;
+	(*data)->many_times_to_eat = times_to_eat;
+	return (EXIT_SUCCESS);
+}
+
+int	init_philos(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	data->philos = malloc(sizeof(t_philo) * (data->num_philos));
+	if (!data->philos)
+	{
+		ft_putendlc_fd(RED_, ALLOC_ERR, 1);
+		return (EXIT_FAILURE);
+	}
+	while (i < data->num_philos)
+	{
+		data->philos[i].id = i;
+		data->philos[i].data = data;
+		data->philos[i].times_eaten = 0;
+		data->philos[i].start_eating = 0;
+		data->philos[i].is_die = 0;
+		data->philos[i].r_fork = 0;
+		data->philos[i].l_fork = 0;
+		pthread_mutex_init(&data->philos[i].mut, NULL);
+		data->philos[i].tid = malloc(sizeof(pthread_t));
+		if (!data->philos[i].tid)
+		{
+			ft_putendlc_fd(RED_, ALLOC_ERR, 1);
+			return (EXIT_FAILURE);
+		}
+		i++;
+	}
+	return (EXIT_SUCCESS);
+}
